6595830eb2a0dd975769c0de0de422c0d714451b
JaberAhmed33 <jaber33357@gmail.com>
2024-12-29 18:56:11 +0200

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,26 +2,20 @@ import { FlatCompat } from "@eslint/eslintrc";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-// Resolve directory and filename for compatibility
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Initialize FlatCompat with the base directory
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Define ESLint configuration array
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"), // Extend Next.js core web vitals
+export default [
+  ...compat.extends("next/core-web-vitals"),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"], // Match JavaScript and TypeScript files
+    files: ["**/*.{js,jsx,ts,tsx}"],
     rules: {
-      "no-unused-vars": "warn", // Warn about unused variables
-      "react/no-unescaped-entities": "warn", // Warn about unescaped entities in React
+      "no-unused-vars": "warn",
+      "react/no-unescaped-entities": "warn",
     },
   },
 ];
-
-// Export the ESLint configuration
-export default eslintConfig;
