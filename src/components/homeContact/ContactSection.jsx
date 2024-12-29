9b8f00f46732f6b835b3919e7bac25151e00cf72
JaberAhmed33 <jaber33357@gmail.com>
2024-12-29 19:59:17 +0200

    first commit

--- src/components/homeContact/ContactSection.jsx
+++ src/components/homeContact/ContactSection.jsx
@@ -8,7 +8,7 @@ export default function ContactSection() {
   const t = useTranslations("common");
 
   return (
-    <div className='bg-[url("/public/bg-contact.jpg")] bg-cover  bg-fixed py-20 relative'>
+    <div className='bg-[url("/bg-contact.jpg")] bg-cover  bg-fixed py-20 relative'>
       <div className="absolute w-full h-full bg-blueShades-600/50 top-0 left-0"/>
       <div className="flex relative border border-borderColor/25 py-10 px-6 z-10 flex-col gap-10 items-center text-white w-11/12 lg:w-[60%] md:w-4/5 mx-auto text-center font-bold">
         <h3 className="text-lg md:text-xl lg:text-2xl">{t("contactUs.title")}</h3>
